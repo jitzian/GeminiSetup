acfa989196bccb5de778bc9a58086441d6ddf692
joNA <jitzian@gmail.com>
2024-02-06 21:02:53 -0500

    gmi jc - Code optimization

--- app/src/main/java/com/org/test/geminisample/weather/mapper/WindLocationToListPointMapper.kt
+++ app/src/main/java/com/org/test/geminisample/weather/mapper/WindLocationToListPointMapper.kt
@@ -4,11 +4,6 @@ import co.yml.charts.common.model.Point
 import javax.inject.Inject
 
 class WindLocationToListPointMapper @Inject constructor() : (List<String>) -> (List<Point>) {
-    override fun invoke(input: List<String>): List<Point> {
-        val points = mutableListOf<Point>()
-        for (i in input.indices) {
-            points.add(Point(i.toFloat(), input[i].toFloat()))
-        }
-        return points
-    }
+    override fun invoke(input: List<String>): List<Point> =
+        input.mapIndexed { index, s -> Point(index.toFloat(), s.toFloat()) }
 }
