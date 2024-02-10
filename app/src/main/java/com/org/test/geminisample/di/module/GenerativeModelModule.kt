1222f9339f99a2f33112202c4bebd712c4d409ad
joNA <jitzian@gmail.com>
2024-02-10 15:55:27 -0500

    gmi jc - Updating safety settings

--- app/src/main/java/com/org/test/geminisample/di/module/GenerativeModelModule.kt
+++ app/src/main/java/com/org/test/geminisample/di/module/GenerativeModelModule.kt
@@ -1,6 +1,9 @@
 package com.org.test.geminisample.di.module
 
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.BlockThreshold
+import com.google.ai.client.generativeai.type.HarmCategory
+import com.google.ai.client.generativeai.type.SafetySetting
 import com.org.test.geminisample.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -13,7 +16,26 @@ class GenerativeModelModule {
     @Reusable
     fun providesGenerativeModule() = GenerativeModel(
         modelName = "gemini-pro",
-        apiKey = BuildConfig.apiKey
+        apiKey = BuildConfig.apiKey,
+        safetySettings = listOf(
+            SafetySetting(
+                HarmCategory.HARASSMENT,
+                BlockThreshold.ONLY_HIGH
+            ),
+            SafetySetting(
+                HarmCategory.HATE_SPEECH,
+                BlockThreshold.LOW_AND_ABOVE
+            ),
+            SafetySetting(
+                HarmCategory.DANGEROUS_CONTENT,
+                BlockThreshold.LOW_AND_ABOVE
+            ),
+            SafetySetting(
+                HarmCategory.SEXUALLY_EXPLICIT,
+                BlockThreshold.LOW_AND_ABOVE
+            ),
+
+        )
     )
 
 }
